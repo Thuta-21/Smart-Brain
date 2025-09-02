d9719970a61e5abc2af9b78bf45ffe81ff5fcfde
Thuta-21 <thutakyaw9125@gmail.com>
2025-09-03 01:03:30 +0630

    changing styles

--- src/components/ParticleBg/Particle.js
+++ src/components/ParticleBg/Particle.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
-
-import ParticlesBg from 'particles-bg'
+import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg';
 
 class ParticleBg extends Component {
+
   render () {
     return (
       <>
-        <ParticlesBg type="cobweb" bg={true} />
+        <ParticlesBg type="polygon" bg={true} />
       </>
     )
   }
